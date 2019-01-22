a5219880e8357674255fe2fc505e33f9220f0aca
Caner Altuner <42742872+caneraltuner@users.noreply.github.com>
2019-01-22 20:01:40 +0300

    Files Uploaded

---
+++ sifreSistemi/sifreSistemi/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace sifreSistemi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string password;
+        public static int textLenght;
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                password = textBox1.Text;
+                textLenght = textBox1.Text.Length;
+                Form2 form2 = new Form2();
+                this.Hide();
+                form2.Show();
+            }
+            else
+            {
+                MessageBox.Show("please fill the textbox");
+            }
+        }
+    }
+}
---
+++ sifreSistemi/sifreSistemi/Form2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace sifreSistemi
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            textBox1.MaxLength = Form1.textLenght;
+            label1.Text = "Password: "+Form1.password;
+            label2.Text = "Current TextBox's TextLenght: "+Form1.textLenght.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text == Form1.password && textBox1.TextLength == Form1.textLenght)
+            {
+                MessageBox.Show("Acess Granted");
+            }
+            else if (textBox1.Text != Form1.password && textBox1.TextLength == Form1.textLenght)
+            {
+                MessageBox.Show("Acess Denied");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
